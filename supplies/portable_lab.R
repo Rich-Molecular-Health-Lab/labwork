e9212e75dfc9db6119126614476638917eb99e30
aliciamrich <aliciamrich@gmail.com>
2025-02-20 17:25:12 -0600

    budget updates

---
+++ supplies/portable_lab.R
@@ -0,0 +1,132 @@
+portable <- list(
+  equipment = list(
+ "mini16X thermal cycler" = list(
+   Link     = "https://www.minipcr.com/product/minipcr-mini8x-thermal-cycler/",
+   Each     = 695.00,
+   Cat      = "QP-1000-08",
+   Manuf    = "miniPCR",
+   Per      = "device",
+   N_year   = 1
+ ),
+ "UVC Sanitizer Ozone Box" = list(
+   Link         = "https://a.co/d/dOtzGYE",
+   Each         = 68.99,
+   Cat          = NULL,
+   Manuf        = "coospider-repta",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "Electrophoresis Setup" = list(
+   Link         = "https://www.minipcr.com/product/bluegel-electrophoresis-with-built-in-illuminator/",
+   Each        = 309.00,
+   Cat      = "QP-1500-01",
+   Manuf = "miniPCR",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "Quantus™ Fluorometer" = list(
+   Link         = "https://www.fishersci.com/us/en/browse/90207111/fluorometers-and-fluorescence-polarization-systems",
+   Each        = 3650.00,
+   Cat      = "E6150",
+   Manuf = "Promega",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "Tube Rotator" = list(
+   Link         = "https://pipette.com/labnet-revolver-adjustable-lab-tube-rotator.html",
+   Each        = 339.56,
+   Cat      = "H5600",
+   Manuf = "Labnet",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "Portable Power Station" = list(
+   Link         = "https://us.ecoflow.com/products/delta-pro-portable-power-station",
+   Each        = 2199.00,
+   Cat      = NULL,
+   Manuf = "EcoFlow",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "BenchMixer™ MP Vortex Mixer" = list(
+   Link         = "https://www.benchmarkscientific.com/product/bv1007/",
+   Each        = 499.00,
+   Cat      = "BV1007",
+   Manuf = "Benchmark Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "Mortexer™ Vortex Mixer" = list(
+   Link         = "https://www.benchmarkscientific.com/product/bv1005/",
+   Each        = 341.70,
+   Cat      = "BV1005",
+   Manuf = "Benchmark Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "-20°C Portable Lab Sample Freezer and Refrigerator" = list(
+   Link         = "https://ivyxscientific.com/collections/freezers/products/minus20freezer",
+   Each        = 399.00,
+   Cat      = NULL,
+   Manuf = "IVYX Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "5L Laboratory Incubator 0°C to +55°C" = list(
+   Link         = "https://ivyxscientific.com/collections/incubators/products/5l-lab-incubator",
+   Each        = 150.00,
+   Cat      = NULL,
+   Manuf = "IVYX Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "12-Place Mini Benchtop Coolers (0)" = list(
+   Link         = "https://www.heathrowscientific.com/12-place-mini-benchtop-coolers/",
+   Each        = 137.00,
+   Cat      = "120070",
+   Manuf = "Heathrow Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "32-Place Mini Benchtop Coolers (-20)" = list(
+   Link         = "https://www.heathrowscientific.com/32-place-mini-benchtop-coolers/",
+   Each        = 282.00,
+   Cat      = "120075",
+   Manuf = "Heathrow Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "Gyro™ Microcentrifuge, fixed speed" = list(
+   Link         = "https://www.minipcr.com/product/gyro-microcentrifuge-fixed-speed/",
+   Each        = 169.00,
+   Cat      = "QP-1800-01",
+   Manuf = "miniPCR",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "MC-12 Microcentrifuge" = list(
+   Link         = "https://www.benchmarkscientific.com/product/c1612/",
+   Each        = 1715.69,
+   Cat      = "C1612",
+   Manuf = "Benchmark Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "BeadBug™3 Microtube homogenizer" = list(
+   Link         = "https://www.benchmarkscientific.com/product/d1030/",
+   Each        = 998.00,
+   Cat      = "D1030",
+   Manuf = "Benchmark Scientific",
+   Per          = "device",
+   N_year       = 1
+ ),
+ "BeadBug™6 Microtube homogenizer" = list(
+   Link         = "https://www.benchmarkscientific.com/product/d1036/",
+   Each        = 2736.67,
+   Cat      = "D1036",
+   Manuf = "Benchmark Scientific",
+   Per          = "device",
+   N_year       = 1
+ )
+)
+  )
