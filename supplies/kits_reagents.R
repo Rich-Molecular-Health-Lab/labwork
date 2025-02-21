1796905d459a5a48b64dadfeb2a3858eadc22a35
aliciamrich <aliciamrich@gmail.com>
2025-02-21 09:52:50 -0600

    budgeting

---
+++ supplies/kits_reagents.R
@@ -0,0 +1,215 @@
+consumables <- list(
+  kits = list(
+    "Quick-DNA Fecal/Soil Microbe Miniprep" = list(
+      Link         = "https://www.zymoresearch.com/collections/quick-dna-fecal-soil-microbe-kits/products/quick-dna-fecal-soil-microbe-dna-miniprep-kit",
+      Each         = 264.90,
+      Cat          = "D6010",
+      Manuf        = "Zymo Research",
+      preps        = 50,
+      samples_prep = 1
+      ),
+    "DNA Clean & Concentrator MagBead Kit" = list(
+      Link         = "https://www.zymoresearch.com/collections/dcc-pcr-purification-kits/products/dna-clean-concentrator-magbead-kit",
+      Each         = 154.90,
+      Cat          = "D4012",
+      Manuf        = "Zymo Research",
+      preps        = 100,
+      samples_prep = 1
+      ),
+    "Qubit 1X dsDNA HS Assay Kit" = list(
+      Link         = "https://www.thermofisher.com/order/catalog/product/Q33230",
+      Each         = 142,
+      Cat          = "Q33230",
+      Manuf        = "Invitrogen",
+      preps        = 100,
+      samples_prep = .25
+    ),
+    "SeeGreen™ All-in-One Agarose Tabs™" = list(
+      Link         = "https://www.minipcr.com/product/seegreen-agarose-tabs-20-tablets/",
+      Each         = 42,
+      Cat          = "RG-1500-20",
+      Manuf        = "miniPCR Bio",
+      preps        = 20,
+      samples_prep = 10
+    ),
+    "REPLI-g UltraFast Mini Kit" = list(
+      Link         = "https://www.qiagen.com/us/products/discovery-and-translational-research/pcr-qpcr-dpcr/preamplification/repli-g-kits?catno=150035",
+      Each         = 1056,
+      Cat          = "150035",
+      Manuf        = "Qiagen",
+      preps        = 100,
+      samples_prep = 1
+    ),
+  "Ligation Sequencing Kit" = list(
+    Link         = "https://store.nanoporetech.com/us/ligation-sequencing-kit-v14.html",
+    Each         = 599,
+    Cat          = "SQK-LSK114",
+    Manuf        = "ONT",
+    preps        = 6,
+    samples_prep = 1
+  ),
+  "16S Barcoding Kit 24" = list(
+    Link         = "https://store.nanoporetech.com/us/16s-barcoding-kit-24-v14.html",
+    Each         = 900,
+    Cat          = "SQK-16S114.24",
+    Manuf        = "ONT",
+    preps        = 6,
+    samples_prep = 24
+  ),
+  "Rapid Barcoding Kit 24" = list(
+    Link         = "https://store.nanoporetech.com/us/rapid-barcoding-sequencing-kit-24-v14.html",
+    Each         = 699,
+    Cat          = "SQK-RBK114.24",
+    Manuf        = "ONT",
+    preps        = 6,
+    samples_prep = 24
+  ),
+  "LongAmp Hot Start Taq 2X Master Mix" = list(
+    Link         = "https://www.neb.com/en-us/products/m0533-longamp-hot-start-taq-2x-master-mix",
+    Each         = 210,
+    Cat          = "M0533",
+    Manuf        = "NEB",
+    preps        = 100,
+    samples_prep = 1
+  ),
+  "NEBNext® Companion Module v2" = list(
+    Link         = "https://www.neb.com/en-us/products/e7672-nebnext-companion-module-oxford-nanopore-technologies-ligation-sequencing",
+    Each         = 752,
+    Cat          = "E7672S",
+    Manuf        = "NEB",
+    preps        = 24,
+    samples_prep = 1
+  ),
+  "T7 Endonuclease I" = list(
+    Link         = "https://www.neb.com/en-us/products/m0302-t7-endonuclease-i",
+    Each         = 80,
+    Cat          = "M0302",
+    Manuf        = "NEB",
+    preps        = 250,
+    samples_prep = 1
+  ),
+  "Blunt/TA Ligase" = list(
+    Link         = "https://www.neb.com/en-us/products/m0367-blunt-ta-ligase-master-mix",
+    Each         = 115,
+    Cat          = "M0367",
+    Manuf        = "NEB",
+    preps        = 50,
+    samples_prep = 1
+)
+),
+flowcells = list(
+  "Flongle" = list(
+    Link         = "https://store.nanoporetech.com/us/flongle-flow-cell-pack-r10-4-1-2.html",
+    Each         = 918,
+    Cat          = "FLO-FLG114",
+    Manuf        = "ONT",
+    preps        = 12,
+    samples_prep = 24
+  ),
+  "MinION" = list(
+    Link         = "https://store.nanoporetech.com/us/flow-cell-r10-4-1-2025.html",
+    Each         = 800,
+    Cat          = "FLO-MIN114",
+    Manuf        = "ONT",
+    preps        = 4,
+    samples_prep = 24
+  )
+),
+reagents = list(
+  "DNA/RNA Shield" = list(
+    Link         = "https://www.zymoresearch.com/collections/bulk-reagents/products/dna-rna-shield",
+    Each         = 255.60,
+    Cat          = "R1100-250",
+    Manuf        = "Zymo Research",
+    preps        = 85,
+    samples_prep = 1
+  ),
+  "Molecular Grade Ethanol" = list(
+    Link         = "https://www.thermofisher.com/order/catalog/product/T038181000",
+    Each         = 176,
+    Cat          = "T038181000",
+    Manuf        = "Thermo Scientific™",
+    preps        = 1000,
+    samples_prep = 1
+  )
+),
+tubes = list(
+  "DNA/RNA Shield Lysis Tubes (Microbe)" = list(
+    Link         = "https://www.zymoresearch.com/collections/vector-and-environmental-collection/products/dna-rna-shield-lysis-and-collection-tubes",
+    Each         = 265.20,
+    Cat          = "R1103",
+    Manuf        = "Zymo Research",
+    preps        = 50,
+    samples_prep = 1
+  ),
+  "2mL Collection Tubes" = list(
+    Link         = "https://www.heathrowscientific.com/2-ml-cryogenic-vials-sterile/",
+    Each         = 256,
+    Cat          = "HS23202A",
+    Manuf        = "Heathrow Scientific",
+    preps        = 500,
+    samples_prep = 1
+  ),
+  "5mL Collection Tubes" = list(
+    Link         = "https://www.globescientific.com/5ml-diamondr-miditm-centrifuge-tubes-with-screw-cap.html?psku=111580S&cid=0",
+    Each         = 132.30,
+    Cat          = "111580BRS",
+    Manuf        = "Globe Scientific",
+    preps        = 500,
+    samples_prep = 1
+  ),
+  "Qubit Assay Tubes" = list(
+    Link         = "https://www.thermofisher.com/order/catalog/product/Q32856",
+    Each         = 119,
+    Cat          = "Q32856",
+    Manuf        = "Invitrogen",
+    preps        = 500,
+    samples_prep = .25
+  ),
+  "1.5mL Microfuge Tubes" = list(
+    Link         = "https://www.zymoresearch.com/collections/tubes/products/dnase-rnase-free-tubes",
+    Each         = 16.40,
+    Cat          = "C2001-100",
+    Manuf        = "Zymo Research",
+    preps        = 100,
+    samples_prep = .5
+  ),
+  "PCR 8-Strips" = list(
+    Link         = "https://pipette.com/mtc-bio-p3010-a.html?Category_Code=pcr-tubes",
+    Each         = 70.80,
+    Cat          = "P3010-A",
+    Manuf        = "MTC-Bio",
+    preps        = 960,
+    samples_prep = .5
+  )
+),
+
+tips = list(
+  "1000 ul" = list(
+    Link         = "https://pipette.com/oxford-lab-products-oar-1000xl-slf.html",
+    Each         = 56.25,
+    Cat          = "OAR-1000XL-SLF",
+    Manuf        = "Oxford Lab Products",
+    preps        = 960,
+    samples_prep = .1
+  ),
+  "200 ul" = list(
+    Link         = "https://pipette.com/oxford-lab-products-pipette-controllers-xr-200-slfc.html",
+    Each         = 260.80,
+    Cat          = "XR-200-SLFC",
+    Manuf        = "Oxford Lab Products",
+    preps        = 4800,
+    samples_prep = .1
+  ),
+  "10 ul" = list(
+    Link         = "https://pipette.com/oxford-lab-products-xr-10n-slf.html",
+    Each         = 57.56,
+    Cat          = "XR-10N-SLF",
+    Manuf        = "Oxford Lab Products",
+    preps        = 960,
+    samples_prep = .1
+  )
+)
+)
+
+
